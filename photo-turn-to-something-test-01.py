fe89d9151e88b4480f21eca88bf004011ca32fa4
Wesley Kay <wesley@cunnymac.local>
2017-03-11 14:21:52 -0700

    working today, plus cleaning out useless file

--- photo-turn-to-something-test-01.py
+++
@@ -1,4 +0,0 @@
-import rhinoscriptsyntax as rs
-
-plane = rs.WorldXYPlane()
-picture = rs.AddPictureFrame(plane,"/Users/wesley/Documents/Me/website/new/links/shopped/JPEG/site-extents-bandw-with-title.jpg")
--- points_project_to_strike_points_differentially001.parameterbynamebreakout.py
+++ points_project_to_strike_points_differentially001.parameterbynamebreakout.py
@@ -1,51 +1,64 @@
 import rhinoscriptsyntax as rs
 import random
-
+import math
+pi = 3.14159
 
 cloud_pts = []
 source_pts = []
 
 def place_pts(x_range, y_range, z_range):
     x = random.uniform(-25, x_range)
-    y = random.uniform(-.2, y_range)
-    z = random.uniform(-.2, z_range)
+    y = random.uniform(-.25, y_range)
+    z = random.uniform(-.25, z_range)
     pt = [x,y,z]
     return pt
 
-#put source point coordinates in source_pts list
+def randX(x_range):
+    x = random.uniform(0,x_range)
+    return x
 
+def randY(y_range):
+    y = random.uniform(0,y_range)
+    return y
+
+def randZ(z_range):
+    z = random.uniform(0,z_range)
+    return z
+
+#setting range for random numbers defined above
+xnum = randX(10)
+ynum = randY(10)
+znum = randZ(10)
 
 #og_source_pts = rs.ObjectsByLayer("Source Points", True)
-og_source_pts = rs.getobjects("Select Source Points", rs.filter.point)
+og_source_pts = rs.GetObjects("Select Source Points", rs.filter.point)
 for pt in og_source_pts:
     ptCoord = rs.PointCoordinates(pt)
     source_pts.append(ptCoord)
     #find layer of each point, put in
     pt_layer_id = rs.ObjectLayer(pt)
 
-#initializing cloud of points
+
+
+#initializing cloud of points, vectorize between cloud and original points, move pt by sub_vect amount\
+#draw line between pt and original points
 for i in range(0,3000):
-    pt = rs.AddPoint(place_pts(25,.2 ,.2))
+    pt = rs.AddPoint(place_pts(25,.25,.25))
     cloud_pts.append(pt)
     #find closest points from cloud to source points
     index = rs.PointArrayClosestPoint(source_pts, pt)
     clos_pt = source_pts[index]
+
     vect = rs.VectorCreate(clos_pt, pt)
     unit_vect = rs.VectorUnitize(vect)
-    sub_vect = vect - unit_vect
+    sub_vect = (vect/2)-(unit_vect)
 
+    #draw line between origin (og_) pts and cloud of random pts
+    init_dst  = rs.AddLine(pt, clos_pt)
+    #move cloud of random pts according to outcome of sub_vect
     new_pt = rs.MoveObject(pt,sub_vect)
-    #vect_line = rs.AddLine(pt,new_pt)
+    sphere_that_line = rs.AddSphere(new_pt,())
     rs.SelectObject(new_pt)
     #print vect
     #print "BREAK"
     #print unit_vect
-
-"""
-    index = rs.PointArrayClosestPoint(pts,pt)
-    cp = pts[index]
-    vect = rs.VectorCreate(cp, pt)
-    unitVect = rs.VectorUnitize(vect)
-    subVect = vect - unitVect
-    newPt = rs.MoveObject(pt,subVect)
-"""
